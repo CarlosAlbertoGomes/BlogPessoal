ad3a9051a79cfa3b100ec3956f3b02bf4d26b086
Carlos Gomes <gomez.3d@hotmail.com>
2023-02-01 18:28:56 -0300

    adc model, repository e controller do meu blog

---
+++ blogPessoalItauApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogPessoalItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class blogPessoalItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(blogPessoalItauApplication.class, args);
+	}
+
+}
