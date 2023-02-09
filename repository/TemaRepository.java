01d043f2451b82ed7d0eb5260e9b5ac2d148bbaa
Carlos Gomes <gomez.3d@hotmail.com>
2023-02-09 14:15:29 -0300

    adc o @Repository no TemaRepository

--- repository/TemaRepository.java
+++ repository/TemaRepository.java
@@ -3,9 +3,10 @@ package com.generation.blogPessoalItau.repository;
 import com.generation.blogPessoalItau.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
     public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
